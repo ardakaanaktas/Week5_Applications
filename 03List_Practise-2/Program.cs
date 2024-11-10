21828fc533b2787290a80e5af80c4aa195769f92
Arda kaan AKTAŞ <arda_kaan_aktas@yahoo.com>
2024-11-10 12:19:04 +0300

    List Practise 2 added

---
+++ 03List_Practise-2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03List_Practise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kahve ismi giriniz (5 adet)");
+
+            List<string> coffes = new List<string>();//Liste oluşturuldu
+
+            for (int i = 0; i < 5; i++)//5 adet kahve ismi alındı
+            {
+                Console.Write($"Kahve {i+1} : ");
+                coffes.Add(Console.ReadLine()); //Kullanıcıdan alınan kahve isimleri listeye eklendi
+            }
+
+            Console.WriteLine("Girilen Kahve isimleri:");
+
+            foreach (var item in coffes)
+            {
+                Console.WriteLine(item); //Listedeki kahve isimleri ekrana yazdırıldı
+            }   
+        }
+    }
+}
