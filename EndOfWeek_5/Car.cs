0bb2e049eb624e9a820f9b40945b9731363c90ee
Arda kaan AKTAŞ <arda_kaan_aktas@yahoo.com>
2024-11-10 21:08:45 +0300

    5. Hafta Kapanış uygulaması eklendi

---
+++ EndOfWeek_5/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EndOfWeek_5
+{
+    public class Car
+    {
+        //Propertyleri tanımladık.
+        public string productionYear { get; set; }
+        public string productionNumber { get; set; }
+        public string brand { get; set; }
+        public string model { get; set; }
+        public string color { get; set; }
+        public string doorNumber { get; set; }
+
+        public Car()
+        {
+            //Constructor oluşturduk.
+            Console.WriteLine("Araba üretildi.");
+            productionYear = DateTime.Now.Year.ToString(); //Şu anın yıl bilgisi alındı.
+            productionNumber = Guid.NewGuid().ToString(); //Guid ile benzersiz bir id oluşturduk.
+        }
+
+
+}
+}
---
+++ EndOfWeek_5/Program.cs
@@ -0,0 +1,73 @@
+﻿namespace EndOfWeek_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Start:
+            Console.WriteLine("Araba üretimi yapıcak mısınız ? E/H");
+            char choose = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            List<Car> cars = new List<Car>();
+            if (choose == 'e'|| choose == 'E')
+            {
+                AddCar:
+                Car car = new Car();
+                DoorCount:
+                Console.Write("Kapı sayısı giriniz: ");
+                var doorNumber = Console.ReadLine();
+                if (doorNumber == "2" || doorNumber == "4")
+                {
+                    car.doorNumber = doorNumber;
+                }
+                else
+                {
+                    Console.WriteLine("Kapı sayısı 2 veya 4 olmalıdır.");
+                    goto DoorCount;//Kapı sayısı 2 veya 4 değilse kullanıcıdan tekrar kapı sayısını alıyoruz.
+                }
+                //Diğer Propertlerini kullanıcıdan alıyoruz.
+                Console.Write("Marka giriniz: ");
+                car.brand = Console.ReadLine();
+
+                Console.Write("Model giriniz: ");
+                car.model = Console.ReadLine();
+
+                Console.Write("Renk giriniz: ");
+                car.color = Console.ReadLine();
+
+                cars.Add(car); //Oluşturulan araba nesnesini listeye ekliyoruz.
+                Console.WriteLine("Başka araç üretmek ister misiniz ? E/H");
+                choose = Console.ReadKey().KeyChar;
+                if (choose == 'e' || choose == 'E')//Eğer kullanıcı başka araç üretmek isterse AddCar'a gidiyor.
+                {
+                    goto AddCar;
+                }
+            }
+            else if (choose == 'h' || choose == 'H')//Eğer kullanıcı araç üretmek istemezse program sonlanıyor
+            {
+                Console.WriteLine("İyi günler dileriz.");
+                goto End;
+            }
+            else
+            {
+                Console.WriteLine("Hatalı giriş yaptınız.");//Kullanıcı yanlış bir tuşa bastıysa başa dönüyor.
+                goto Start;
+            }
+
+        End:
+            Console.WriteLine("Üretilen araçlar: ");//Üretilen araçların bilgilerini yazdırıyoruz.
+            foreach (var item in cars)
+            {
+                Console.WriteLine("Üretim yılı: " + item.productionYear);
+                Console.WriteLine("Seri numarası: " + item.productionNumber);
+                Console.WriteLine("Marka: " + item.brand);
+                Console.WriteLine("Model: " + item.model);
+                Console.WriteLine("Renk: " + item.color);
+                Console.WriteLine("Kapı sayısı: " + item.doorNumber);
+                Console.WriteLine("-------------------------------------------------");
+            }
+            Console.WriteLine("Program sonlandı.");
+
+        }
+    }
+}
