f2a931b170ebb48adafc26eb07b589fcdb803bfb
Arda kaan AKTAŞ <arda_kaan_aktas@yahoo.com>
2024-11-10 12:14:10 +0300

    List Practise Added

---
+++ 03List_Practise-1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _03List_Practise_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new List<string>();
+            list.Add("Bülent Ersoy");
+            list.Add("Ajda Pekkan");
+            list.Add("Ebru Gündeş");
+            list.Add("Hadise");
+            list.Add("Hande Yener");
+            list.Add("Tarkan");
+            list.Add("Funda Arar");
+            list.Add("Demet Akalın");
+
+            Console.WriteLine("** Davetliler **");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ 03Lists/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _03Lists
+{
+    //Generic Listeler farklı veri tipleriyle dinamik olarak çalışabilmeyi sağlar. Dizilere göre daha esnek bir yapıya sahiptir.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();//Boş bir int listesi tanımladık.
+            numbers.Add(1);//Listeye eleman ekledik.
+            numbers.Add(2);
+            numbers.Add(3);
+
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            numbers.Insert(1, 10);//1. indexe 10 sayısını ekledik.
+
+            bool isnumberExist  = numbers.Contains(10);//Listede 10 sayısı var mı kontrolü yapar. True ya da False döner.
+
+            numbers.Remove(10);//Listeden 10 sayısını sildik.
+
+            numbers.RemoveAt(0);//0. indexdeki elemanı sildik.
+
+            numbers.Clear();//Listeyi temizledik.
+
+            numbers.AddRange(new int[] { 1, 2, 3, 4, 5 });//Dizi elemanlarını listeye ekledik.
+
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+
+            numbers.Sort();//Listeyi küçükten büyüğe sıralar.
+
+            numbers.Reverse();//Listeyi tersine çevirir.
+
+            foreach (var item in numbers)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
